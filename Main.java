28e974e772059b78756f72cf1b63eec97b94f4aa
tanyyaako <tanyyaako@icloud.com>
2024-11-28 11:09:02 +0300

    Add Main 2

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("Это добавлено в ветке main 1");
+        System.out.print("Это добавлено в ветке main 2");
     }
 }
